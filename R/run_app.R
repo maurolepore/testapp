2f9cfd99c0ef1fa6be7aa1b87a132a89691c2ec8
Mauro Lepore <maurolepore@gmail.com>
2024-07-12 19:30:56 +0000

    Document

---
+++ R/run_app.R
@@ -0,0 +1,38 @@
+#' @import shiny
+NULL
+
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examplesIf interactive()
+#' run_app()
+run_app <- function(path = ".") {
+  from <- function(path) {
+    sprintf("read_parquet('%s/**/*.parquet', hive_partitioning = true)", path)
+  }
+
+  ui <- fluidPage(
+    textInput("path", "Path to explore", value = path),
+    textInput("db", "Path read", value = NULL),
+    numericInput(".n", "Rows to show", min = 1, max = 10, value = 5),
+    verbatimTextOutput("explore"),
+    tableOutput("table")
+  )
+  server <- function(input, output, session) {
+    output$explore <- renderText({
+      list.files(input$path)
+    })
+
+    output$table <- renderTable({
+      req(input$db, input$.n)
+
+      con <- DBI::dbConnect(duckdb::duckdb(), dbdir = ":memory:")
+      dataset <- dplyr::tbl(con, from = from(input$db))
+      utils::head(dataset, input$.n)
+    })
+  }
+
+  shinyApp(ui, server)
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# testapp
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of testapp is to provide the UI to work with data hosted
+elsewhere.
+
+## Installation
+
+``` r
+install.packages("pak")
+pak::pak("maurolepore/testapp")
+```
+
+## Example
+
+    testapp::run_app("/")
